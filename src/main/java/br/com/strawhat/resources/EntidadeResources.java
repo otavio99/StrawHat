232b2901c3e7e43802f3e6f884cf546929229bee
Otavio Nathanael <otavio.nathanael@gmail.com>
2020-05-18 04:40:17 +0100

    desbilitando cors para EntidadesResources

--- src/main/java/br/com/strawhat/resources/EntidadeResources.java
+++ src/main/java/br/com/strawhat/resources/EntidadeResources.java
@@ -16,23 +16,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import br.com.strawhat.model.Entidade;
 import br.com.strawhat.dto.EntidadeDTO;
 import br.com.strawhat.services.EntidadeService;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping(value="/entidades")
 public class EntidadeResources {
 	@Autowired
 	private EntidadeService service;
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id){
 		Entidade associado = service.find(id);
 		return ResponseEntity.ok().body(associado);
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@Valid @RequestBody EntidadeDTO objDto){
 		Entidade obj = service.fromDTO(objDto);
@@ -41,7 +42,7 @@ public class EntidadeResources {
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody EntidadeDTO objDto, @PathVariable Integer id){
 		Entidade obj = service.fromDTO(objDto);
@@ -49,26 +50,26 @@ public class EntidadeResources {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<EntidadeDTO>> findAll() {
 		List<Entidade> list = service.findAll();
 		List<EntidadeDTO> listDTO = list.stream().map(obj -> new EntidadeDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
-	
+
 	@RequestMapping(value = "/page",method = RequestMethod.GET)
 	public ResponseEntity<Page<EntidadeDTO>> findPage(
-			@RequestParam(value = "page", defaultValue = "0") Integer page, 
-			@RequestParam(value = "linesPerPage", defaultValue = "24")Integer linesPerPage, 
-			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
-			@RequestParam(value = "direction", defaultValue = "ASC") String direction) 
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24")Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction)
 	{
 		Page<Entidade> list = service.findPage(page, linesPerPage, orderBy, direction);
 		Page<EntidadeDTO> listDTO = list.map(obj -> new EntidadeDTO(obj));
